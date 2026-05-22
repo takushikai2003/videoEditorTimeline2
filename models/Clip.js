8632da434a8fd4a52b2e5908ea7b6429991e04df
takushikai <takushikai@gmail.com>
2026-05-23 02:42:19 +0900

    add files

---
+++ models/Clip.js
@@ -0,0 +1,74 @@
+// クリップの基本クラス
+export class Clip {
+    constructor({ id, name, startTime, duration, color }) {
+        this.name = name; // 表示名
+        this.startTime = startTime;
+        this.endTime = startTime + duration;
+        this.color = color;
+    }
+
+    // 内部データを出力
+    getInternalData() {
+        return {
+            id: this.id,
+            startTime: this.startTime,
+            endTime: this.endTime,
+            color: this.color,
+        };
+    }
+}
+
+// ビデオトラック用（relativeStartTime や gain を保持）
+export class VideoClip extends Clip {
+    constructor(data) {
+        super(data);
+        this.mediaId = data.mediaId;
+        this.relativeStartTime = data.relativeStartTime || 0;
+        this.gain = data.gain || 1;
+    }
+
+    getInternalData() {
+        return {
+            ...super.getInternalData(),
+            mediaId: this.mediaId,
+            relativeStartTime: this.relativeStartTime,
+            gain: this.gain
+        };
+    }
+}
+
+// オーディオトラック用（relativeStartTime や gain を保持）
+export class AudioClip extends Clip {
+    constructor(data) {
+        super(data);
+        this.mediaId = data.mediaId;
+        this.relativeStartTime = data.relativeStartTime || 0;
+        this.gain = data.gain || 1;
+    }
+
+    getInternalData() {
+        return {
+            ...super.getInternalData(),
+            mediaId: this.mediaId,
+            relativeStartTime: this.relativeStartTime,
+            gain: this.gain
+        };
+    }
+}
+
+// エフェクトトラック用
+export class EffectClip extends Clip {
+    constructor(data) {
+        super(data);
+        this.effectId = data.effectId;
+        this.effectParams = data.effectParams; // エフェクトの内部パラメータ（文字の色など）
+    }
+
+    getInternalData() {
+        return {
+            ...super.getInternalData(),
+            effectId: this.effectId,
+            effectParams: this.effectParams,
+        };
+    }
+}
